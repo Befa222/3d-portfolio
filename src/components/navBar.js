fadba5ac844a3cb34a749e6340b17280d9164998
Befa222 <72620186+Befa222@users.noreply.github.com>
2021-06-15 18:22:35 +0100

    startding dropdown menu

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,33 +1,35 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import '../style/navBar.scss'
 import { Link } from 'react-router-dom'
 
 
 export default function NavBar() {
 
-    const [projectsList, setProjectList] = useState(false)
+    const [list, setList] = useState(false)
 
+    function DropDown() {
+        (list) ? setList(false) : setList(true)
+    }
 
     return (
 
         <div className='navBar'>
-        <Link to='/Home'><h1 id='initials'>FL</h1></Link> 
+            <Link to='/Home'><h1 id='initials'>FL</h1></Link>
             <ul>
                 <Link to='/About-me'><li>About</li></Link>
                 <Link to='/Contact-me'><li>contact</li></Link>
-                <li className='ppp'>projects</li>
-                
-                    <div >
-                        <ul className='listP'>
-                        <li>Pick a Chew</li>
-                        <li>Tempera</li>
-                        <li>Weather App</li>
-                        <li>Wild Universe</li>
-                        </ul>
-                    </div>
-                
+                <li className='ppp' onClick={DropDown}>projects</li>
                 <Link to='/Credits-to-artists'><li>credits</li></Link>
+
             </ul>
+            {list && (
+                <div className='nav-project-list'>
+                    <h2>Pick a Chew</h2>
+                    <h2>Tempera</h2>
+                    <h2>Weather app</h2>
+                    <h2>Wild universe</h2>
+                </div>
+            )}
         </div>
 
     )
